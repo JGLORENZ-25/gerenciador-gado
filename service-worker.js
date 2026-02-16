f3638e8fdff8830e1a5067a2da5f13b934542c35
JGLORENZ-25 <69226135+JGLORENZ-25@users.noreply.github.com>
2026-02-16 14:46:59 -0300

    Update service worker cache version to V13

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meugado-V12-final'; // Mudei a versão para forçar atualização no navegador
+const CACHE_NAME = 'meugado-V13-final'; // Mudei a versão para forçar atualização no navegador
 
 // Apenas arquivos LOCAIS que garantimos que existem na pasta
 const ASSETS_TO_CACHE = [
@@ -77,4 +77,5 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
